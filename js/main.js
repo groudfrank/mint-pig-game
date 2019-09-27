2579db26e3feba0d39297b83d594f672a94500bb
Groud Frank <groudfrank@gmail.com>
2019-09-27 15:42:38 -0400

    Started working on some JS and got the dice and numerical to change when the roll dice button is clicked. I'll work on better visual feedback and animation after I get everything working the way it should

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function(){
 
-}); //Document
+var rollDiceBtn = document.getElementById('roll-dice-btn');
+var dice = document.getElementById('dice');
+var numericalDiceValue = document.getElementById('numerical-dice-value');
+var rollValue, numToWord, numToKey, newClassName;
+var diceValues = {
+        1:'one',
+        2:'two', 
+        3:'three',
+        4:'four',
+        5:'five',
+        6:'six'
+};
+
+function changeDice(number){
+    numToWord = diceValues[number];
+    dice.className = "fas fa-dice-" + numToWord;
+    numericalDiceValue.innerHTML = number;
+}
+
+rollDiceBtn.addEventListener('click', function(){
+    rollValue = Math.floor((Math.random() * 6) + 1);
+    changeDice(rollValue);
+});
+
+
+
+});
